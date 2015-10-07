f05065ff15191536ec50c5eccf014ac45c44ab70
peaceandiago <paola.santiago@mail.utoronto.ca>
2015-10-06 23:52:14 -0400

    Added variables for Assignment 1 Finished Assignment 1 - you can double check and we can see if we can add more comments

--- exercise1.py
+++ exercise1.py
@@ -12,22 +12,27 @@ __email__ = "ses@drsusansim.org"
 __copyright__ = "2015 Susan Sim"
 __license__ = "MIT License"
 
+stock = 2000
+bought = 900.00
+commission = .03
+share_sold = 942.75
 
-share = 2000
-price per share = 900.00
-stockbroker commission = .03
-#she sold all her shares
-price per share sold = 942.75
+#first half of the question
+initial_purchase = stock * bought
+print (initial_purchase)
+initial_sales_commission = initial_purchase*commission
+print (initial_sales_commission)
+initial_amount_of_money = initial_sales_commission + initial_purchase
+print (initial_amount_of_money)
 
-print(Lakshmi_remaining)
-# input the number of shares
-
-# input initial price per share
-
-# subtract 3% commission to pay her stockbroker
-
-# calculate new price for shares
-
-# subtract 3% commission from the new price for the shares
-
-# calculate if Lakshmi made a profit
+#second half
+stock_sold = stock * share_sold
+print (stock_sold)
+final_sales_commission = stock_sold * commission
+print (final_sales_commission)
+overall_cost = initial_amount_of_money + final_sales_commission
+print (overall_cost)
+#Using the overall cost and comparing with stock sold to se it Lakshmi profited or lost money
+result = stock_sold - overall_cost
+print (result)
+#with -$25,065, Lakshmi lost money
