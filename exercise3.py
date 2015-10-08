a5c91afd52afba38d03f5e3a8a430281c71dc355
kating26 <kating26@gmail.com>
2015-10-07 23:50:50 -0400

    Got rid of extra Y(es) statement and made sure left side works first.

--- exercise3.py
+++ exercise3.py
@@ -15,14 +15,14 @@ __license__ = "MIT License"
 
 def diagnose_car():
 #Start on the left side
-    troubleshoot = raw_input("Is the car silent when you turn the key?")
-    if troubleshoot == " yes": #Added a space for aesthetics
-        positive_first_degree = raw_input("Are the battery terminals recorded?")
-        print (positive_first_degree)
-        if positive_first_degree == " yes": #I don't understand why I'm getting a double yes
-            print ("Clean terminals and try starting again.")
-        if positive_first_degree == " no": #there is another no
-            print ("Replace cables and try again.")
+    troubleshoot = raw_input("Is the car silent when you turn the key (Y/N)? ")
+    if troubleshoot == "Y": #Added a space for aesthetics
+        positive_first_degree = raw_input("Are the battery terminals corroded (Y/N)? ")
+    if positive_first_degree == "Y": #Got rid of extra print line, removed extra "Y"
+        print ("Clean terminals and try starting again.")
+    if positive_first_degree == "N": #there is another no
+        print ("Replace cables and try again.")
+"""
 #Start on the right side
     if troubleshoot == " no":
         negative_first_degree = raw_input("Does the car make a clicking noise?")
@@ -30,7 +30,7 @@ def diagnose_car():
         if negative_first_degree == " yes":
             print ("Replace the battery.")
         if negative_first_degree == " no": #This is a bit tricky
-
+"""
 
 
 
