7c1655474ca6ae36716b337ff623dca6f2e1e66f
kating26 <kating26@gmail.com>
2015-10-07 22:24:42 -0400

    Code for exercise2 for your approval

--- exercise2.py
+++ exercise2.py
@@ -12,24 +12,38 @@ __email__ = "ses@drsusansim.org"
 __copyright__ = "2015 Susan Sim"
 __license__ = "MIT License"
 
-
+# User inputs the number of sides and returns shapes.
+shape = raw_input("Enter the number of sides for your shape: ")
 
 def name_that_shape():
-
     """
     For a given number of sides in a regular polygon, returns the shape name
 
-    Inputs:
+    Inputs: Integers from 3 to 10. Users can enter other characters and numbers.
 
-    Expected Outputs:
+    Expected Outputs: Name of each shape based on the number of sides input by user from 3 to 10.
+    3 - triangle, 4 - quadrilateral, 5 - pentagon, 6 - hexagon, 7 - heptagon, 8 - octagon,
+    9 - nonagon, 10 - decagon
 
-    Errors:
-
+    Errors: Any number outside the range of 3-10 (inclusive) as well as symbol and characters
+    will result in an "error".
     """
-    shape = raw_input("Let's name that shape")
-    if shape =="3":
-        print ('triangle')
 
-
-
-
+if shape =="3":
+    print ('You have a triangle.')
+elif shape == "4":
+    print ('You have a quadrilateral.')
+elif shape == "5":
+    print ('You have a pentagon.')
+elif shape == "6":
+    print ('You have a hexagon.')
+elif shape == "7":
+    print ('You have a heptagon.')
+elif shape == "8":
+    print ('You have an octagon.')
+elif shape == "9":
+    print ('You have a nonagon.')
+elif shape == "10":
+    print ('You have a decagon.')
+else:
+    print ('Error')
